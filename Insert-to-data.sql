955d84dc0a9242278b578a377b7140c8be171b40
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2024-10-25 12:14:17 +0530

    Insert to data

---
+++ Insert-to-data.sql
@@ -0,0 +1,131 @@
+INSERT INTO user(u_nic,email,f_name,l_name,address,gender)
+VALUES
+
+
+ student1('200123567856','kasun.bandara07@gmail
+.com','kasun','bandara','123/4, Temple Road, Kandy ','M'),
+
+ student2('200202676389','nadeesha.ekanayake23@gmail
+.com','Nadeesha','Ekanayake','78, Station Road, Galle ','F'),
+
+ student3('200220803567','dilini.jayawardena88@gmail
+.com','Dilini','Jayawardana','No. 32, Green Lane, Nugegoda ','F'),
+
+ student4('200290898983','lahiru.sadaruwan15@gmail
+.com','Lahiru','Sadaruwan','56/7A, Lake Road, Nuwara Eliya ','M'),
+
+ student5('200023676276','sajith.gunaratne56@gmail
+.com','Sajiith','Madushan','21, Park Avenue, Ratnapura','M'),
+
+ student6('200245357872','harsha.weerasinghe34@gmail
+.com','Harsha','Devappriya','89, Hill Street, Matara','M'),
+
+ student7('200266989943','tharindu.rathnayake09@gmail
+.com',' Tharindu','Madushan','No. 101, Church Road, Negombo','M'),
+
+ student8('200256789387','chathurika.abeysinghe77@gmail
+.com','Chathurika','Abeyasinghe','34/2, Riverside Road, Anuradhapura','F'),
+
+student9('200223537858','lakshman.karumar68@gmail.com','lakshman','kumar','No. 45, Station Road, Badulla','M'),
+
+student10('200256687748','thushara.jayasinghe271@gmail.com','Thushara','jayasinghe','55, Market Road, Ampara','M'),
+
+repeater1('200123564765','supun.chamod126@gmail.com','Supun','Chamod','67, New Town Road, Polonnaruwa ','M'),
+
+repeater2('200078774675','gayantha.fernando91@gmail.com','Gayantha','Fernando','9/1, Post Office Road, Kurunegala','M'),
+
+repeate3('200066455876','ruwanthi.desilva30@gmail.com','Ruwanthi','Desilva','No. 23, College Avenue, Trincomalee','F'),
+
+repeater4('200123645434','nisansala.samarasinghe81@gmail.com','Nisansala','samarasinghe','No. 72, High Street, Kalutara','F'),
+
+repeater5('200123546384','chamara.maduranga2001@gmail.com','Chamara','Maduranga','No. 90, Temple Road, Puttalam','M'),
+
+
+
+
+to1('983223897V','sandun.silva03@gmail.com.com','kasun','bandara','No. 5, School Lane, Jaffna','M'),
+
+to2('995623567V','krishan.amarasinhe50@gmail.com','krishan','Amarasinhe','No. 41, Circular Road, Mannar','M'),
+
+to3('983889364V','hansamal.witharana50@gmail.com','Hansamal','Witharana','83, School Lane, Dehiwala','M');
+
+to4('987865786V','namal.kumara564@gmail.com','Namal','Kumara','No. 28, Lake Drive, Gampaha','M'),
+
+to5('990623567V','isuru.dealwis04@gmail.com','Isuru','dealwis','14, Park View, Kegalle','M'),
+
+
+
+
+lec1('628776349V','nuwantha.wickramasinghe45@gmail.com','Nuwantha','wickramasinghe','48/2, Kandy Road, Chilaw ','M'),
+
+lec2 ('978467895V','kumari.hettiarachchi44@gmail.com','Kumari','hettiarachchi','12/3B, Beach Road, Batticaloa','F'),
+
+lec3('995123567V','menaka.kodithuwakku59@gmail.com','Menaka','kodithuwakku','No. 19, Railway Avenue, Hambantota','M'),
+
+lec4('986523567V','malini.gamage18@gmail.com','Malini','Gamage','34/5, Queens Road, Vavuniya','F'),
+
+lec5('985727664V','piumi.abeykoon50@gmail.com','Piumi','abeykoon','75/1, St. Marys Road, Moratuwa','F'),
+
+lec6 ('976458123V', 'kasun.senanayaka07@gmail.com', 'Kasun', 'Senanayaka', 'No. 12/1, Kandy Road, Kurunegala', 'M')
+
+lec7 ('875412369V', 'nadeesha.ekanayake23@gmail.com', 'Nadeesha', 'Ekanayake', 'No. 24/A, Dharmapala Mawatha, Gampaha', 'F'),
+
+lec8 ('945621378V', 'dilini.jayawardena88@gmail.com', 'Dilini', 'Jayawardena', 'No. 78, Lake Drive, Nuwara Eliya', 'F'),
+
+lec9 ('893246517V', 'lahiru.perera15@gmail.com', 'Lahiru', 'Perera', 'No. 36, Old Matara Road, Galle', 'M'),
+
+lec10 ('852369741V', 'lakmal.gunaratne56@gmail.com', 'Lakmal', 'Gunaratne', 'No. 102, Hospital Road, Batticaloa', 'M');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+INSERT INTO user_phone (p_u_nic, u_phone)
+VALUES
+    
+    ('200123567856', '0712345678'),
+    ('200202676389', '0761234569'),
+    ('200220803567', '0702345671'),
+    ('200290898983', '0723456782'),
+    ('200023676276', '0741234563'),
+    ('200245357872', '0751234564'),
+    ('200266989943', '0783456785'),
+    ('200256789387', '0713456786'),
+    ('983223897V', '0772345677'),
+    ('978467895V', '0704567890'),
+    ('200223537858', '0752345678'),
+    ('200123564765', '0783456789'),
+    ('200078774675', '0714567890'),
+    ('200066455876', '0765678901'),
+    ('628776349V', '0726789012'),
+    ('200123645434', '0757890123'),
+    ('200256687748', '0788901234'),
+    ('995123567V', '0719012345'),
+    ('986523567V', '0700123456'),
+    ('200123546384', '0761234567'),
+    ('990623567V', '0772345678'),
+    ('995623567V', '0743456789'),
+    ('985727664V', '0714567890'),
+    ('987865786V', '0755678901'),
+    ('983889364V', '0776789012'),
+    ('976458123V', '0772345678'),
+    ('875412369V', '0783456789'),
+    ('945621378V', '0714567890'),
+    ('893246517V', '0765678901'),
+    ('852369741V', '0706789012');
+    
+
+
+
+
+
