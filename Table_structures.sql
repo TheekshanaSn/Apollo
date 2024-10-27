b63e3503d109c1d157485c3318bc7ecc670f52a0
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2024-10-27 17:41:27 +0530

    update-to-table-structures

--- Table_structures.sql
+++ Table_structures.sql
@@ -6,7 +6,7 @@
     f_name VARCHAR(20),
     l_name VARCHAR(20), 
     /*ADD*/ address VARCHAR (50),
-   /*ADD*/ gender  CHAR(1),
+   /*ADD*/ gender CHAR(1),
     PRIMARY KEY(u_nic)
    
 );
@@ -22,15 +22,20 @@ CREATE TABLE user_phone
 
 CREATE TABLE attendance
 (
-    attendance_id VARCHAR(15),
-    type VARCHAR(10) NOT NULL,
-   /*not now*/ eligibility VARCHAR(8) NOT NULL,
-    date DATE NOT NULL,
-    student_id VARCHAR(15) NOT NULL,
-    to_id VARCHAR(15) NOT NULL,
-    PRIMARY KEY(attendance_id),
-    FOREIGN KEY (student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+    -- attendance_id VARCHAR(15),
+    at_student_id VARCHAR(15),
+    at_course_code VARCHAR(15),
+    type(T/P) VARCHAR(10),
+--    /*not now*/ eligibility VARCHAR(8) NOT NULL,
+    date DATE ,
+    medical_status VARCHAR(15),
+    session_no INT NOT NULL,
+    to_id VARCHAR(15),
+    PRIMARY KEY(at_student_id,at_course_code),
+    FOREIGN KEY (at_student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (at_course_code) REFERENCES course_unit(course_code) ON DELETE CASCADE,
     FOREIGN KEY (to_id) REFERENCES technical_officer(to_id) ON DELETE CASCADE
 );
+
 
 
