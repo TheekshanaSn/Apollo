8bb77f0d2d59cc48fb2a03e2f1df9691650a7f43
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2024-10-31 01:40:24 +0530

    changes

--- Table_structures.sql
+++ Table_structures.sql
@@ -11,7 +11,7 @@
    
 );
 
-CREATE TABLE User_Phone
+CREATE TABLE user_phone
 (
     p_u_nic VARCHAR(15),
     u_phone VARCHAR(15),
@@ -20,7 +20,6 @@ CREATE TABLE User_Phone
     
 );
 
-<<<<<<< Updated upstream
 
 CREATE TABLE Medical(
      medical_id  VARCHAR(15),
@@ -30,45 +29,21 @@ CREATE TABLE Medical(
      FOREIGN KEY(med_student_id) REFERENCES student(student_id) ON DELETE CASCADE
 );
 
-=======
->>>>>>> Stashed changes
 CREATE TABLE Attendance
 (
     attendance_id VARCHAR(15),
     at_student_id VARCHAR(15),
     at_course_code VARCHAR(15),
     at_course_type(T/P) VARCHAR(10),
---    /*not now*/ eligibility VARCHAR(8) NOT NULL,
-<<<<<<< Updated upstream
     date DATE,
     attendence VARCHAR(20),
     medical_status VARCHAR(15),
-=======
-    date DATE ,
-    attendance VARCHAR(15),
->>>>>>> Stashed changes
     session_no INT NOT NULL,
-    at_to_id VARCHAR(15),
-    PRIMARY KEY(attendance_id),
+    to_id VARCHAR(15),
+    PRIMARY KEY(at_student_id,at_course_code),
     FOREIGN KEY (at_student_id) REFERENCES student(student_id) ON DELETE CASCADE,
-<<<<<<< Updated upstream
     FOREIGN KEY (at_course_code,at_course_type) REFERENCES course_unit(course_code,type) ON DELETE CASCADE,
-=======
-    FOREIGN KEY (at_course_code) REFERENCES course_unit(course_code) ON DELETE CASCADE
->>>>>>> Stashed changes
     FOREIGN KEY (to_id) REFERENCES technical_officer(to_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE Medical
-(
-medical_id VARCHAR(15),
-med_student_id VARCHAR(15),
--- tech_officer_id VARCHAR(15),
-date  DATE,
-PRIMARY KEY (medical_id),
-FOREIGN KEY (med_student_id) REFERENCES student(student_id),
--- FOREIGN KEY (tech_officer_id) REFERENCES technical_officer(to_id)
 );
 
 
