594b69250a2891b7e974fb15da9a58d41c8080dd
Thishan Herath <diniduthishan9@gmail.com>
2024-10-31 22:22:43 +0530

    course of student

--- Query.sql
+++ Query.sql
@@ -3,3 +3,22 @@ CREATE VIEW lecture_detail AS
 SELECT l.lecturer_id, l.department, l.lc_nic, u.email, u.f_name, u.l_name, u.address, u.gender
 FROM lecturer l INNER JOIN user u
 ON l.lc_nic = u.u_nic;
+
+select * from lecture_detail;
+
+
+--------------------course of student----------------------
+DELIMITER //
+
+CREATE PROCEDURE course_student(IN c_code VARCHAR(10))
+BEGIN
+SELECT su.student_id, CONCAT(u.f_name, ' ' , u.l_name) AS "Student Name"
+FROM student_course_unit su INNER JOIN student s
+ON su.student_id = s.student_id
+INNER JOIN user u ON s.st_nic = u.u_nic
+WHERE su.course_code = c_code;
+END //
+
+DELIMITER ;
+
+call course_student('ICT1212');
