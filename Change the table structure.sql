7cc7287f5477e2be6fb8b76f8a617e6cef410571
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2024-10-31 21:40:39 +0530

    change the attendance value

--- Change the table structure.sql
+++ Change the table structure.sql
@@ -1,3 +1,30 @@
 
 ALTER TABLE attendance
 CHANGE attendence attendance VARCHAR(20);
+
+
+UPDATE attendance 
+SET attendance='absences' 
+WHERE Attendance_id ="AT0280" OR 
+      Attendance_id ="AT0290" OR
+      Attendance_id ="AT0334" OR
+      Attendance_id ="AT0335" OR
+      Attendance_id ="AT0336" OR
+      Attendance_id ="AT0337" OR
+      Attendance_id ="AT0490" OR
+      Attendance_id ="AT0493" OR
+      Attendance_id ="AT0504" OR
+      Attendance_id ="AT0500" OR
+      Attendance_id ="AT0604" OR
+      Attendance_id ="AT0607" OR
+      Attendance_id ="AT0704" OR
+      Attendance_id ="AT0790" OR
+      Attendance_id ="AT0820" OR
+      Attendance_id ="AT0946" OR
+      Attendance_id ="AT0947" OR
+      Attendance_id ="AT0950" OR
+      Attendance_id ="AT0972" OR
+      Attendance_id ="AT0976" OR
+      Attendance_id ="AT1000" ;
+
+
