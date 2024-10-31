5fd8000425c1783867774f8db3ba024d79557da0
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2024-11-01 03:41:46 +0530

    add-to-procedure-and-view

--- Attendance-Related-Function.sql
+++ Attendance-Related-Function.sql
@@ -10,7 +10,6 @@ GROUP BY at_student_id,at_course_code
 HAVING ((COUNT( Attendance_ID))*100/15)>=80 ORDER BY at_student_id;
 
 
-
 --All studence Attendance Only Pactical above 80 present without Medical
 SELECT at_student_id,at_course_code,(COUNT( Attendance_id))/15*100 AS 'Attendance percentage'
 FROM Attendance 
@@ -25,6 +24,40 @@ FROM Attendance
 WHERE Attendance = 'Present' AND at_course_type='TP'
 GROUP BY at_student_id,at_course_code
 HAVING ((COUNT( Attendance_ID))*100/15)>=80 ORDER BY at_student_id;
+
+
+
+
+
+-- 80% > attendance_percentage student---->
+
+
+--All studence Attendance Only Theory less 80 present without Medical
+
+SELECT at_student_id,at_course_code,(COUNT( Attendance_id))/15*100 AS 'Attendance percentage'
+FROM Attendance 
+WHERE Attendance = 'Present' AND at_course_type='T'
+GROUP BY at_student_id,at_course_code
+HAVING ((COUNT( Attendance_ID))*100/15)<80 ORDER BY at_student_id;
+
+
+
+--All studence Attendance Only Pactical less 80 present without Medical
+SELECT at_student_id,at_course_code,(COUNT( Attendance_id))/15*100 AS 'Attendance percentage'
+FROM Attendance 
+WHERE Attendance = 'Present' AND at_course_type='P'
+GROUP BY at_student_id,at_course_code
+HAVING ((COUNT( Attendance_ID))*100/15)<80 ORDER BY at_student_id;
+
+
+--All studence Attendance Theory and Pactical less 80 present without Medical
+SELECT at_student_id,at_course_code,(COUNT( Attendance_id))/15*100 AS 'Attendance percentage'
+FROM Attendance 
+WHERE Attendance = 'Present' AND at_course_type='TP'
+GROUP BY at_student_id,at_course_code
+HAVING ((COUNT( Attendance_ID))*100/15)<80 ORDER BY at_student_id;
+
+
 
 
 
@@ -62,8 +95,11 @@ GROUP BY  at_Student_id,at_course_code ORDER BY at_student_id;
 CREATE OR REPLACE VIEW attendance_theory_and_pactical AS 
 SELECT at_Student_id,at_course_code,COUNT(attendance_id)/15*100 AS 'Attendance percentage'
 FROM attendance
-WHERE at_course_type='T/P' AND (attendance='present' OR medical_status IS NOT NULL)
+WHERE at_course_type='TP' AND (attendance='present' OR medical_status IS NOT NULL)
 GROUP BY  at_Student_id,at_course_code ORDER BY at_student_id;
+
+
+
 
 
 
@@ -103,7 +139,7 @@ HAVING ((COUNT( Attendance_ID))/15*100)>=80 ORDER BY at_student_id;
 
 -- CREATE PROCEDURE Function to view attendance by Student
 DELIMITER //
-CREATE PROCEDURE attendance_for_subject ( student VARCHAR(15),type VARCHAR(10) )
+CREATE PROCEDURE attendance_for_student ( student VARCHAR(15),type VARCHAR(10) )
 BEGIN
 SELECT at_student_id,at_course_code,COUNT( attendance_id)/15*100 AS 'Percentage',
 IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
@@ -113,8 +149,25 @@ GROUP BY at_course_code;
 End//
 DELIMITER ;
 
+CALL attendance_for_student('TG1366','T');
 
-CALL attendance_for_subject('TG1366','T');
+
+
+-- CREATE PROCEDURE Function to view attendance subject by subject 
+DELIMITER //
+CREATE PROCEDURE attendance_for_subject ( subject VARCHAR(15),type VARCHAR(10) )
+BEGIN
+SELECT at_course_code,at_student_id,COUNT( attendance_id)/15*100 AS 'Percentage',
+IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
+FROM attendance
+WHERE at_course_code=subject AND at_course_type=type  AND (attendance='Present' OR medical_status IS NOT NULL)
+GROUP BY at_Student_id;
+End//
+DELIMITER ;
+
+CALL attendance_for_subject('ICT1212','T');
+
+
 
 
 
@@ -150,3 +203,48 @@ GROUP BY at_student_id,at_course_code;
 
 
 
+--as individuals by giving the registration no..
+
+--as individuals by giving the registration no only theory
+
+SELECT at_student_id,at_course_code,(COUNT( attendance_id))/15*100 AS 'percentage',
+IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
+FROM attendance 
+WHERE at_course_type="T" AND (attendance = 'Present' OR medical_status IS NOT NULL) AND at_Student_id='TG1398'
+GROUP BY at_course_code,at_Student_id;
+
+--as individuals by giving the registration no only pactical
+
+SELECT at_student_id,at_course_code,(COUNT( attendance_id))/15*100 AS 'percentage',
+IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
+FROM attendance 
+WHERE at_course_type="P" AND (attendance = 'Present' OR medical_status IS NOT NULL) AND at_Student_id='TG1398'
+GROUP BY at_course_code,at_Student_id;
+
+--as individuals by giving the registration no only theory and pactical
+
+SELECT at_student_id,at_course_code,(COUNT( attendance_id))/15*100 AS 'percentage',
+IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
+FROM attendance 
+WHERE at_course_type="TP" AND  (attendance = 'Present' OR medical_status IS NOT NULL) AND at_Student_id='TG1398'
+GROUP BY at_course_code,at_Student_id;
+
+
+
+
+
+-- CREATE PROCEDURE Function to view attendance by Student
+DELIMITER //
+CREATE PROCEDURE attendance_for_student_and_course ( student VARCHAR(15),subject VARCHAR(15),type VARCHAR(10) )
+BEGIN
+SELECT at_student_id,at_course_code,COUNT( attendance_id)/15*100 AS 'Percentage',
+IF((COUNT( attendance_id)/15*100)>=80,"Eligible","Not eligible") AS 'Eligiblity'
+FROM attendance
+WHERE at_student_id=student AND at_course_code=subject AND at_course_type=type AND (attendance='Present' OR medical_status IS NOT NULL)
+GROUP BY at_course_code;
+End//
+DELIMITER ;
+
+
+CALL attendance_for_student_and_course('TG1366','ICT1212','T');
+CALL attendance_for_student_and_course('TG1353','ICT1222','P');
